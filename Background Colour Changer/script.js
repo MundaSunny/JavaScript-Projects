b5e4aa02758cf3e4531cfef0bbe3aec93e8eca87
Sunny <mundasunny99@gmail.com>
2026-03-23 21:34:49 +0530

    updated

--- Background Colour Changer/script.js
+++ Background Colour Changer/script.js
@@ -3,3 +3,12 @@ function changeBackgroundColor() {
             const randomColor = colors[Math.floor(Math.random() * colors.length)];
             document.body.style.backgroundColor = randomColor;
         }
+
+    const colorButton = document.getElementById('colorButton');
+    colorButton.addEventListener('click', (e) => {
+        // console.log(e.target);
+        const child= e.target;
+        const body= document.querySelector('body');
+        body.style.backgroundColor= child.id ;
+        
+    });
